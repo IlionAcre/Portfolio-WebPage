10bcc28be6327b967a4b01aee9e9d61f5c5390b4
Acre <10746503+IlionAcre@users.noreply.github.com>
2024-04-13 21:28:21 -0500

    Fixed vars

--- main.py
+++ main.py
@@ -1,46 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, flash
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 import os
+from dotenv import load_dotenv
+import requests
 
-SCOPES = ["https://www.googleapis.com/auths/spreadsheets.readonly"]
-DATA_ID = "1mP3-B9zY-167B8QsgPrSOL5zafDvDtERxYobg3TQaeE"
+load_dotenv()
+headers = {
+    "Authorization" : os.getenv('PT')
+}
+print(os.getenv("SP"))
+skills = requests.get(os.getenv("SP"), headers=headers).text
+projects = requests.get(os.getenv("PP"), headers=headers).text
 
-credentials = None
-if os.path.exists("./db/credentials.json"):
-    credits = Credentials.from_authorized_user_file("./db/token.json", SCOPES)
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.flow_client_secrets_file("./db/credentials.json", SCOPES)
-            credentials = flow.run_local_server(port=0)
-        with open("./db/token.json", "w") as token:
-            token.write(credentials.to_json())
-
-        try:
-            service = build("sheets", "v4", credentials=credentials)
-            sheets = service.spreadsheets()
-
-            result = sheets.values().get(spreadsheetId=DATA_ID, range="Sheet1!A1:C4").execute()
-
-            values = result.get("values", [])
-
-            for row in values:
-                print(row)
-
-        except HttpError as error:
-            print(error)
+print(skills)
+print(projects)
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", skills=skills, projects=projects)
 
 @app.route('/projects')
 def projects():
